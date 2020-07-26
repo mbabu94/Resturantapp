74a915983802d7d7fafad1716afc1ba773047d94
mbabu94 <mahmudbabu94@gmail.com>
2020-07-25 22:04:28 -0400

    Added more imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import Home from './Components/Home';
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Menu</h1>
+        <Home />
         <h1>About</h1>
         <h1>Contact</h1>
 
